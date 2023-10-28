340d056768ac04b9e09164a2131191a1c4682d0c
joseph <oliseh3@gmail.com>
2023-10-28 16:46:52 -0400

    homework

---
+++ training/Employee.java
@@ -0,0 +1,11 @@
+public class Employee {
+    String dress;
+    String salary;
+    String deduction;
+    String bonus;
+
+    public void dressing() {
+        System.out.println("wear a suit to work ");
+    }
+
+}
---
+++ training/Joseph.java
@@ -0,0 +1,67 @@
+
+
+public class Joseph {
+    public static void main(String[] args) {
+
+//        u = u +5;
+//       String name= well +" "+welll;
+//
+//        Character po = 'P';
+//        System.out.println(goat);
+//        System.out.println(wk);
+//        System.out.println(name);
+
+//        Scanner input= new Scanner(System.in);
+//        System.out.println("Enter your score");
+//        double score = input.nextDouble();
+//        System.out.println("result");
+//        if(score >75){
+//            System.out.println("A");
+//        } else if (score > 65 && score < 75) {
+//            System.out.println("B");
+//        } else if (score > 50 && score <65) {
+//            System.out.println("C");
+//        } else if (score >30 && score <50) {
+//            System.out.println("D");
+//        }else{System.out.println("F");}
+
+
+        int[] record = new int (30);
+        int[] student ={30,28,7,25}
+        record[0]=1;
+        record[1]=2;
+        record[2]=3;
+        record[3]=4;
+        record[4]=5;
+        record[5]=6;
+        record[6]=7;
+        record[7]=8;
+        record[8]=9;
+        record[9]=10;
+        record[10]=11;
+        record[11]=12;
+        record[12]=13;
+        record[13]=14;
+        record[14]=15;
+        record[15]=16;
+        record[16]=17;
+        record[17]=18;
+        record[18]=19;
+        record[19]=20;
+        record[20]=21;
+        record[21]=22;
+        record[22]=23;
+        record[23]=24;
+        record[24]=25;
+        record[25]=26;
+        record[26]=27;
+        record[27]=28;
+        record[28]=29;
+        record[29]=30;
+        System.out.println(Arrays.toString(record));
+        System.out.println(student );
+
+    }
+
+    }
+
